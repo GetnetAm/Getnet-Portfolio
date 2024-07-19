5cfe765540200e812d8d91c5dd81fdd41b95eb2d
GetnetAm <getnetamsalu2119@gmail.com>
2024-07-19 11:39:18 -0700

    footer page design

---
+++ src/pages/footer/Footer.js
@@ -0,0 +1,21 @@
+
+import "./footer.css";
+
+
+function Footer() {
+  return (
+    <div className="container">
+          <div className="footer">
+      <div className="footer-content">
+
+        <div className="copy-right">
+          <p>@Copyright All Rights Reserved</p>
+        </div>
+      </div>
+    </div>
+
+    </div>
+  );
+}
+
+export default Footer;
