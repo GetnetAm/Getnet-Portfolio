b6517ffc65559730b1d1bd9e8556addb16e9d210
GetnetAm <getnetamsalu2119@gmail.com>
2024-07-19 11:28:38 -0700

    design main Portfolio

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './components/scroller/ScrollToTop';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter >
+  <React.StrictMode>
+  <ToastContainer />
+  < ScrollToTop />
+    <App />
+  </React.StrictMode>
+  </BrowserRouter>
+);
+
+
